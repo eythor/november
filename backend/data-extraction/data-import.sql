3eb23001da96d6a9128d362ebbaf8d092589bdb2
Eythor Sigmundsson <eythor88@gmail.com>
2025-11-29 11:51:31 +0100

    Add data import sql script

---
+++ backend/data-extraction/data-import.sql
@@ -0,0 +1,17 @@
+.mode csv
+.import extracted-csv/allergyintolerance.csv allergy_intolerances
+.import extracted-csv/careteam.csv care_teams
+.import extracted-csv/claim.csv claims
+.import extracted-csv/condition.csv conditions
+.import extracted-csv/diagnosticreport.csv diagnostic_reports
+.import extracted-csv/documentreference.csv document_references
+.import extracted-csv/encounter.csv encounters
+.import extracted-csv/imagingstudy.csv imaging_studies
+.import extracted-csv/immunization.csv immunizations
+.import extracted-csv/location.csv locations
+.import extracted-csv/medicationrequest.csv medication_requests
+.import extracted-csv/observation.csv observations
+.import extracted-csv/organization.csv organizations
+.import extracted-csv/patient.csv patients
+.import extracted-csv/practitioner.csv practitioners
+.import extracted-csv/procedure.csv procedures
